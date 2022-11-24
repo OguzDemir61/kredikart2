e36e91511d20c8e4a68abeac22bf81e9f77231e1
OguzDemir61 <demiroguzhuseyinsafa@gmail.com>
2022-11-24 16:09:24 +0300

    credit card project
    credit card information entryq screen

---
+++ java.js
@@ -0,0 +1,33 @@
+/* Kart Numarası Boşluğu */
+
+$("#card-number").on("keypress change blur", function () {
+  $(this).val(function (index, value) {
+    return value.replace(/[^a-z0-9]+/gi, "").replace(/(.{4})/g, "$1 ");
+  });
+});
+
+$("#card-number").on("copy cut paste", function () {
+  setTimeout(function () {
+    $("#card-number").trigger("change");
+  });
+});
+
+/* Tarih Çizgisi */
+
+$("#card-exp").on("input", function () {
+  var curLength = $(this).val().length;
+  if (curLength === 2) {
+    var newInput = $(this).val();
+    newInput += "/";
+    $(this).val(newInput);
+  }
+});
+
+ var characterCount
+ $('#card-exp').on('input',function(e){
+     if($(this).val().length == 2 && characterCount < $(this).val().length) {
+        $(this).val($(this).val()+'/');
+     }
+     characterCount = $(this).val().length });
+ console.log("Oğuz DEMİR")
+     
